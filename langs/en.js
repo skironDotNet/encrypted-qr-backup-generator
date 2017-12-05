2ab1a156c28fc0018e5fbf5e5fe1af91ff89c494
Pawel Cioch <pawel@greenhex.net>
2017-12-05 15:07:50 -0600

    some UI steps and code fixes

--- langs/en.js
+++ langs/en.js
@@ -1,10 +1,13 @@
 var translations = {
     cPass: 'Your Passphrase',
     cPassConfirm: 'Confirm Passphrase',
-    cPlainSecret: 'Your Secret to Encrypt',
+    cPlainTextLabel: 'Your Secret to Encrypt',
     cEncQrData: 'QR Data',
     cQrLabel: 'QR Label',
     cQrDescription: 'QR Description',
-    cOptional: '(Optional)',
+    cOptional: '(Optional)<i>test</i>',
     cGenerate: 'Generate',
+    cEmbedLabel: 'Embed Label in the QR',
+
+    hxPlainTextHint: 'Private key(s) or seed(s) or anything wish to encrypt. Exeeding 1024 characters may result in QR being hard to recognize by ceratian software.',    
 }
