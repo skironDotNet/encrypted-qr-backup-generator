02bdeed77442f187170f90c87950577582095ecc
Pawel Cioch <pawel@greenhex.net>
2017-12-28 19:46:25 -0600

    default lang code defined as file:/// won't store cookie

--- langs/lang-map.js
+++ langs/lang-map.js
@@ -1,10 +1,13 @@
+//set your default lang here for offline persistance. Cookies does not for file:/// so it must be set here
+var defaultLanguageCode = "en";
+
 var langsAvailable = [
-	{
-		Name: 'English',
-		Code: 'en'
-	},
-	{
-		Name: 'Polski',
-		Code: 'pl'
-	},
-]
+  {
+    Name: "English",
+    Code: "en"
+  },
+  {
+    Name: "Polski",
+    Code: "pl"
+  }
+];
